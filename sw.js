480e52252290e75ee1c1e48e75e271a3bd14abcd
ramunaspetraska-debug <ramunaspetraska@gmail.com>
2026-06-26 18:01:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Kešo versija suderinta su programos versija (v212 — 2026-06-19)
-const CACHE_NAME = 'superpadel-cache-v229';
+const CACHE_NAME = 'superpadel-cache-v230';
 
 // Visi ekosistemos resursai, kurie privalo veikti neprisijungus prie interneto
 const ASSETS = [
